f58d5156701da25c75ab4017c3d2dc3e4147c77f
Jack Dwyer <jdwy215@gmail.com>
2019-11-25 13:40:12 -0500

    addin destructing examples

--- annotationsFuncsNObjs/funcs.ts
+++ annotationsFuncsNObjs/funcs.ts
@@ -6,4 +6,26 @@ function subtract(x: number = 1, y: number = 2): number {
   return x - y;
 }
 
+const logger = (...values: Array<any>): void => {
+  console.log(...values);
+};
+
+logger("purp", 23, "e");
+
 console.log(add(12));
+
+const weather = {
+  date: new Date(),
+  temp: 72,
+};
+
+const logWeather = ({ date, temp }: { date: Date; temp: number }): void => {
+  console.log(`${date}: ${temp}`);
+};
+
+const { date }: { date: Date } = weather;
+// or just const { date }: { date: Date } = weather;
+
+console.log(date);
+
+logWeather(weather);
