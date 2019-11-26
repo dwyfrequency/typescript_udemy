4637767ce9edb5a94acab1a4da88f8e4eb885ca2
Jack Dwyer <jdwy215@gmail.com>
2019-11-26 12:51:54 -0500

    adding private method

--- classes/classes.ts
+++ classes/classes.ts
@@ -1,20 +1,20 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
-  }
-
-  honk(): void {
+  public honk(): void {
     console.log("beep");
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
   }
 }
 
 const car = new Car();
 
-car.drive();
+car.startDrivingProcess();
 car.honk();
