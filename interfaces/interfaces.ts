70233c5cdee2cd6b5168b3635428c6a7545dab5a
Jack Dwyer <jdwy215@gmail.com>
2019-11-26 11:37:04 -0500

    adding func

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -2,12 +2,18 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  summary(): string;
 }
 
 const oldcivic = {
   name: "civic",
   year: 2000,
   broken: true,
+  summary() {
+    return `Name: ${this.name}`;
+  },
 };
 
-const printVehicle = (vehicle: Vehicle): void => console.log(vehicle);
+const printVehicle = (vehicle: Vehicle): void => console.log(vehicle.summary());
+
+printVehicle(oldcivic);
