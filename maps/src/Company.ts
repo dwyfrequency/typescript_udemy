6b444bed8d4d66b777e1b3f989b4f7cbcdf90849
Jack Dwyer <jdwy215@gmail.com>
2019-11-28 23:34:30 -0500

    adding index and company

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,5 +1,16 @@
 import faker from "faker";
 
 export class Company {
-  constructor() {}
+  companyName: string;
+  catchPhrase: string;
+  location: { lat: number; lon: number };
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: Number(faker.address.latitude()),
+      lon: Number(faker.address.longitude()),
+    };
+  }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,9 @@
 import { User } from "./User";
+import { Company } from "./Company";
+
 console.log("hello");
 const user = new User();
+const company = new Company();
 
 console.log(user);
+console.log(company);
