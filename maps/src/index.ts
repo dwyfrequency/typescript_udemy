6b8edd4a5bb9741cabbbb8ab9edd6ebba6de643f
Jack Dwyer <jdwy215@gmail.com>
2019-11-28 23:59:11 -0500

    adding more ts

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -4,14 +4,9 @@ import { Company } from "./Company";
 console.log("hello");
 const user = new User();
 const company = new Company();
+import { CustomMap } from "./CustomMap";
 
 console.log(user);
 console.log(company);
 
-new google.maps.Map(document.querySelector("#root"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+new CustomMap();
