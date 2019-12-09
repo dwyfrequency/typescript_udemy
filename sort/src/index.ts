593abb3430a474a069ea566312b6b1e9d7ea0c1e
dwyfrequency <jdwy215@gmail.com>
2019-12-09 16:05:27 -0500

    adding implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,10 +1,33 @@
 class Sorter {
   constructor(public collection: number[]) {}
 
-  sort(): void {}
+  private swap(idx1: number, idx2: number): void {
+    const temp = this.collection[idx1];
+    this.collection[idx1] = this.collection[idx2];
+    this.collection[idx2] = temp;
+  }
+
+  sort(): void {
+    /*  */
+    const { length } = this.collection;
+    let swapped: boolean;
+    do {
+      swapped = false;
+      for (let j = 0; j < length - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          this.swap(j, j + 1);
+          swapped = true;
+        }
+      }
+    } while (swapped);
+  }
 }
 
 const sorter = new Sorter([10, 4, -5, 0]);
 
+const sorter1 = new Sorter([10, 4, 11, 0]);
+
 sorter.sort();
+sorter1.sort();
 console.log(sorter.collection);
+console.log(sorter1.collection);
